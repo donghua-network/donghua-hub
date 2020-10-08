42f7174d766c541ec5f98fc0ada337690c5c5602
Fuseteam <fusekai@outlook.com>
2020-10-08 12:42:32 -0300

    move log out to the controller

--- www/controller/logout.php
+++ www/controller/logout.php
@@ -3,5 +3,5 @@ require_once('includes/config.php');
 if ( isset($_SESSION['discord']) ) {
    session_destroy();
 }
-website::website_redirect($website['url']);
+\controller\website::website_redirect($website['url']);
 ?>
