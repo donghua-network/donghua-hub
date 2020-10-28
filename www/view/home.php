38abc9bb3db8a62ed1164dbc60153b98fcff34f6
Fuseteam <fusekai@outlook.com>
2020-10-28 09:37:12 -0300

    add disqus comments

--- www/view/home.php
+++ www/view/home.php
@@ -1,12 +1,3 @@
 <?php
-$form=new model\form('columns',array('Name'=>'text','Type'=>'text'));
-if($_POST['view']=='home'){
-    echo '<form id="date" class="form" method="POST" action="controller/ajax">
-            <div class="form-fields">'.
-                $form->formInputFields().
-            '</div>
-                <input class="btn btn-dark my-2 my-sm-0 save-btn" value="save" type="submit">
-                <input class="btn btn-secondary my-2 my-sm-0 reset-btn" value="reset" type="reset">
-          </form>';
-}
+require('services/disqus.html');
 ?>
